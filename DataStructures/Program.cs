27a435b61c6994c5a47f8569cbd22a9c4d57167c
Jack.Hsu <jackhsu0225@gmail.com>
2021-07-11 15:42:38 -0400

    Add StackQueue

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -7,25 +7,13 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            var queue = new ArrayQueue(3);
-            queue.Enqueue(1);
-            queue.Enqueue(1);
-            queue.Enqueue(1);
-            queue.Enqueue(1);
-            queue.Dequeue();
-            Console.WriteLine(queue.ToString());
-            queue.Dequeue();
-            Console.WriteLine(queue.ToString());
-            queue.Dequeue();
-            Console.WriteLine(queue.ToString());
-            queue.Dequeue();
-            Console.WriteLine(queue.ToString());
-            queue.Enqueue(2);
-            Console.WriteLine(queue.ToString());
-            queue.Enqueue(2);
-            Console.WriteLine(queue.ToString());
-            queue.Enqueue(2);
-            Console.WriteLine(queue.ToString());
+            var que = new StackQueue();
+
+            que.Enqueue(1);
+            que.Enqueue(2);
+            que.Enqueue(3);
+            que.Dequeue();
+            Console.WriteLine(que.Peek());
         }
     }
 }
---
+++ DataStructures/Queues/StackQueue.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Queues
+{
+    public class StackQueue
+    {
+        private Stack<int> _first;
+        private Stack<int> _second;
+
+        public StackQueue()
+        {
+            _first = new Stack<int>();
+            _second = new Stack<int>();
+        }
+
+        public void Enqueue(int item)
+        {
+            _first.Push(item);
+        }
+
+        public int Dequeue()
+        {
+            Flush();
+            return _second.Count == 0 ? throw new InvalidOperationException() : _second.Pop();
+        }
+
+        public bool IsEmpty()
+        {
+            return _first.Count == 0 && _second.Count == 0;
+        }
+
+        public int Peek()
+        {
+            Flush();
+            return _second.Count == 0 ? throw new InvalidOperationException() : _second.Peek();
+        }
+
+        private void Flush()
+        {
+            if (_second.Count == 0)
+            {
+                while (_first.Count != 0)
+                    _second.Push(_first.Pop());
+            }
+        }
+
+        public override string ToString()
+        {
+            Flush();
+            var builder = new StringBuilder();
+            builder.Append("[");
+
+            while(_second.Count != 0)
+            {
+                builder.Append(_second.Pop());
+                if (_second.Count != 0)
+                    builder.Append(", ");
+            }
+
+            builder.Append("]");
+
+            return builder.ToString();
+        }
+    }
+}

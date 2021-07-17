3897621d699c4c03916ea0c3a7afa1a1238d988a
Jack.Hsu <jackhsu0225@gmail.com>
2021-07-17 14:08:14 -0400

    Add StafckWithTwoQueue

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -8,22 +8,6 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            var que = new PriorityQueue(5);
-            que.Enqueue(1);
-            que.Enqueue(5);
-            que.Enqueue(2);
-            que.Enqueue(0);
-            que.Enqueue(10);
-            Console.WriteLine(que.ToString());
-
-            var q = new Queue<int>();
-            q.Enqueue(1);
-            q.Enqueue(2);
-            q.Enqueue(3);
-            q.Enqueue(4);
-            q.Enqueue(5);
-            var rq = new QueueReverser();
-            rq.Reverse(q, 0);
         }
     }
 }

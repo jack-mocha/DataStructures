c43a488450d4aa2472a2b0d77ee9a514406327ae
Jack.Hsu <jackhsu0225@gmail.com>
2021-08-10 23:12:49 -0400

    Add GetStockSpan. Add GetStockSpanUsingStack

---
+++ DataStructures/Stacks/StockSpan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Stacks
+{
+    public class StockSpan
+    {
+        public int[] GetStockSpanUsingStack(int[] prices)
+        {
+            var indices = new Stack<int>();
+            var span = new int[prices.Length];
+            span[0] = 1; //first is always 1
+            indices.Push(0); //first index
+
+            for(int i = 1; i < prices.Length; i++)
+            {
+                while (indices.Count > 0 && prices[indices.Peek()] < prices[i])
+                    indices.Pop();
+
+                span[i] = indices.Count == 0 ? i + 1 : i - indices.Peek();
+                indices.Push(i);
+            }
+
+            return span;
+        }
+
+        public int[] GetStockSpan(int[] prices)
+        {
+            var span = new int[prices.Length];
+            span[0] = 1;
+
+            for(int i = 1; i < prices.Length; i++)
+            {
+                span[i] = 1;
+                for (int j = i - 1; j >= 0 && prices[j] <= prices[i]; j--)
+                    span[i]++;
+            }
+
+            return span;
+        }
+    }
+}

132651c37e03c6e84b731c0641a943f01f083e1e
Jack.Hsu <jackhsu0225@gmail.com>
2021-08-10 23:41:48 -0400

    Add NextGreaterElement and unit tests

---
+++ DataStructures.UnitTests/Stacks.UnitTests/StockSpanUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using DataStructures.Stacks;
+using NUnit.Framework;
+
+namespace DataStructures.UnitTests.Stacks.UnitTests
+{
+    public class StockSpanUnitTests
+    {
+        [Test]
+        [TestCase(new int[] { 4,5,2,25 }, "[5, 25, 25, -1]")]
+        [TestCase(new int[] { 13,7,6,12 }, "[-1, 12, 12, -1]")]
+        public void NextGreaterElement_WhenCalled_ReturnNextGreaterElements(int[] input, string expectedResult)
+        {
+            var e = new StockSpan();
+
+            var elements = e.NextGreaterElement(input);
+            var result = e.Print(elements);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
--- DataStructures/Stacks/StockSpan.cs
+++ DataStructures/Stacks/StockSpan.cs
@@ -15,7 +15,7 @@ namespace DataStructures.Stacks
 
             for(int i = 1; i < prices.Length; i++)
             {
-                while (indices.Count > 0 && prices[indices.Peek()] < prices[i])
+                while (indices.Count > 0 && prices[indices.Peek()] <= prices[i])
                     indices.Pop();
 
                 span[i] = indices.Count == 0 ? i + 1 : i - indices.Peek();
@@ -38,6 +38,41 @@ namespace DataStructures.Stacks
             }
 
             return span;
+        }
+
+        public int[] NextGreaterElement(int[] numbers)
+        {
+            var res = new int[numbers.Length];
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                var next = -1;
+                for(int j = i + 1; j < numbers.Length; j++)
+                {
+                    if(numbers[j] > numbers[i])
+                    {
+                        next = numbers[j];
+                        break;
+                    }
+                }
+                res[i] = next;
+            }
+            return res;
+        }
+
+        public string Print(int[] input)
+        {
+            var builder = new StringBuilder();
+            builder.Append("[");
+            for(int i = 0; i < input.Length; i++)
+            {
+                builder.Append(input[i]);
+                if (i != input.Length - 1)
+                    builder.Append(", ");
+            }
+            builder.Append("]");
+
+            return builder.ToString();
         }
     }
 }

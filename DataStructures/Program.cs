de80178daade38852d3d1dd20b77f448017b70f2
Jack.Hsu <jackhsu0225@gmail.com>
2021-08-18 23:19:54 -0400

    Add Reverse. Add ReverseFirstKElements

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -8,6 +8,10 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
+            var o = new QueueOps();
+            var q = new Queue<int>(new int[] { 1, 2, 3, 4, 5 });
+            o.ReverseFirstKElements(q, 3);
+            o.Print(q);
         }
     }
 }
---
+++ DataStructures/Queues/QueueOps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Queues
+{
+    public class QueueOps
+    {
+        public void ReverseFirstKElements(Queue<int> queue, int k)
+        {
+            var stk = new Stack<int>();
+            for (int i = 0; i < k; i++)
+                stk.Push(queue.Dequeue());
+
+            while (stk.Count > 0)
+                queue.Enqueue(stk.Pop());
+
+            for (int i = 0; i < queue.Count - k; i++)
+                queue.Enqueue(queue.Dequeue());
+        }
+
+        public void Reverse(Queue<int> queue)
+        {
+            if(queue.Count > 0)
+            {
+                var temp = queue.Dequeue();
+                Reverse(queue);
+                queue.Enqueue(temp);
+            }
+        }
+
+        public void Print(Queue<int> queue)
+        {
+            var builder = new StringBuilder();
+            builder.Append("[");
+            foreach (var q in queue)
+                builder.Append(q + " ");
+
+            builder.Append("]");
+            Console.WriteLine(builder.ToString());
+        }
+    }
+}

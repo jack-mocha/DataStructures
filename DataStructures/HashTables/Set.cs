36a92e198dae6d8fc565c1006886f60432517b69
Jack.Hsu <jackhsu0225@gmail.com>
2021-08-28 14:20:16 -0400

    Add IsSubSet to Set.cs

---
+++ DataStructures/HashTables/Set.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.HashTables
+{
+    public class Set
+    {
+        public bool IsSubSet(int[] arr1, int[] arr2)
+        {
+            var set = new HashSet<int>();
+            foreach (var a in arr2)
+                set.Add(a);
+
+            foreach (var a in arr1)
+            {
+                if (set.Contains(a))
+                    set.Remove(a);
+            }
+
+            return set.Count == 0;
+        }
+    }
+}

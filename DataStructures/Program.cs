26e7f7b77ae0bbbcbfd5815a0a99ab57e24f6151
Jack.Hsu <jackhsu0225@gmail.com>
2021-09-07 23:23:43 -0400

    Add BST. Add Insert and InsertRecursive. Add Find and FindRecursive

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,3 +1,4 @@
+using DataStructures.Trees;
 using System;
 
 namespace DataStructures
@@ -6,6 +7,15 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
+            var tree = new BinarySearchTree();
+            tree.Insert(7);
+            tree.Insert(4);
+            tree.Insert(9);
+            tree.Insert(1);
+            tree.Insert(6);
+            tree.Insert(8);
+            tree.Insert(10);
+            Console.WriteLine(tree.Find(10));
         }
     }
 }
---
+++ DataStructures/Trees/BinarySearchTree.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Trees
+{
+    public class BinarySearchTree
+    {
+        private Node _root;
+
+        private class Node
+        {
+            public int Value { get; private set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+
+            public Node(int value)
+            {
+                Value = value;
+            }
+
+            public override string ToString()
+            {
+                return "Node=" + Value;
+            }
+        }
+
+        public void Insert(int value)
+        {
+            var node = new Node(value);
+            if (_root == null)
+            {
+                _root = node;
+                return;
+            }
+
+            var current = _root;
+            while(current != null)
+            {
+                if (value > current.Value)
+                {
+                    if (current.Right != null)
+                        current = current.Right;
+                    else
+                    {
+                        current.Right = node;
+                        return;
+                    }
+                }
+                else
+                {
+                    if (current.Left != null)
+                        current = current.Left;
+                    else
+                    {
+                        current.Left = node;
+                        return;
+                    }
+                }
+            }
+        }
+
+        public void InsertRecursive(int value)
+        {
+            _root = DoInsertRecursive(value, _root);
+        }
+
+        private Node DoInsertRecursive(int value, Node node)
+        {
+            if (node == null)
+                return new Node(value);
+
+            if (value > node.Value)
+                node.Right = DoInsertRecursive(value, node.Right);
+            else
+                node.Left = DoInsertRecursive(value, node.Left);
+
+            return node;
+        }
+
+        public bool Find(int value)
+        {
+            var current = _root;
+            while(current != null)
+            {
+                if (value > current.Value)
+                    current = current.Right;
+                else if (value < current.Value)
+                    current = current.Left;
+                else
+                    return true;
+            }
+
+            return false;
+        }
+
+        public bool FindRecursive(int value)
+        {
+            return DoFindRecursive(value, _root);
+        }
+
+        private bool DoFindRecursive(int value, Node node)
+        {
+            if (node == null)
+                return false;
+
+            if (value > node.Value)
+                return DoFindRecursive(value, node.Right);
+            if (value < node.Value)
+                return DoFindRecursive(value, node.Left);
+            else
+                return true;
+        }
+    }
+}

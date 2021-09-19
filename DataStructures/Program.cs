109514ae99f41c6a2baf7605347d921fc8d6357d
Jack.Hsu <jackhsu0225@gmail.com>
2021-09-18 22:32:28 -0400

    Add Heap Insert() and Remove()

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -7,20 +7,13 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            var tree1 = new BinarySearchTree();
-            tree1.Insert(10);
-            tree1.Insert(20);
-            tree1.Insert(5);
-            tree1.Insert(7);
-            tree1.Insert(2);
-            tree1.Insert(25);
-            var res = tree1.IsPerfect();
-            Console.WriteLine(res);
-            //tree1.Insert(1);
-            //tree1.Insert(6);
-            //tree1.Insert(8);
-            //tree1.Insert(10);
-            //tree1.Insert(11);
+            var heap = new Heap();
+            heap.Insert(10);
+            heap.Insert(5);
+            heap.Insert(17);
+            heap.Insert(4);
+            heap.Insert(22);
+            heap.Remove();
         }
     }
 }
---
+++ DataStructures/Trees/Heap.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Trees
+{
+    public class Heap
+    {
+        private int[] _items = new int[10];
+        private int _size;
+
+        public int Remove()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException();
+
+            var root = _items[0];
+            _items[0] = _items[--_size];
+
+            BubbleDown();
+
+            return root;
+        }
+
+        private void BubbleDown()
+        {
+            var index = 0;
+            while (index <= _size && !IsValidParent(index))
+            {
+                var largerChildIndex = LargerChildIndex(index);
+                Swap(index, largerChildIndex);
+                index = largerChildIndex;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return _size == 0;
+        }
+
+        private int LargerChildIndex(int index)
+        {
+            if (!HasLefChild(index))
+                return index;
+
+            if (!HasRightChild(index))
+                return LeftChildIndex(index);
+
+            return LeftChild(index) > RightChild(index) ? LeftChildIndex(index) : RightChildIndex(index);
+        }
+
+        private bool HasLefChild(int index)
+        {
+            return LeftChildIndex(index) <= _size;
+        }
+
+        private bool HasRightChild(int index)
+        {
+            return RightChildIndex(index) <= _size;
+        }
+
+        private bool IsValidParent(int index)
+        {
+            if (!HasLefChild(index))
+                return true;
+
+            var isValid = _items[index] >= LeftChild(index);
+
+            if (HasRightChild(index))
+                 isValid = _items[index] >= RightChild(index);
+
+            return isValid;
+        }
+
+        private int LeftChild(int index)
+        {
+            return _items[LeftChildIndex(index)];
+        }
+
+        private int RightChild(int index)
+        {
+            return _items[RightChildIndex(index)];
+        }
+
+        private int LeftChildIndex(int index)
+        {
+            return index * 2 + 1;
+        }
+
+        private int RightChildIndex(int index)
+        {
+            return index * 2 + 2;
+        }
+
+        public void Insert(int value)
+        {
+            if (IsFull())
+                throw new InvalidOperationException();
+
+            _items[_size++] = value;
+
+            BubbleUp();
+        }
+
+        public bool IsFull()
+        {
+            return _size == _items.Length;
+        }
+
+        private void BubbleUp()
+        {
+            var index = _size - 1;
+            while (index > 0 && _items[index] > _items[Parent(index)])
+            {
+                Swap(index, Parent(index));
+                index = Parent(index);
+            }
+        }
+
+        private int Parent(int index)
+        {
+            return (index - 1) / 2;
+        }
+
+        private void Swap(int first, int second)
+        {
+            var temp = _items[first];
+            _items[first] = _items[second];
+            _items[second] = temp;
+        }
+    }
+}

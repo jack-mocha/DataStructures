80be63749a9ef201f713a22f68c5f4a7e29e8b6c
Jack Hsu <jackhsu0225@gmail.com>
2022-04-26 14:53:14 -0400

    Add Disjoint Set - FastFind

---
+++ DataStructures/DisjointSet/FastFind.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace DataStructures.DisjointSet
+{
+    public class FastFind
+    {
+        private int[] _vertices;
+        public FastFind(int size)
+        {
+            _vertices = new int[size];
+            for (int i = 0; i < _vertices.Length; i++)
+                _vertices[i] = i;
+        }
+
+        //Time: O(1)
+        public int Find(int x)
+        {
+            return _vertices[x];
+        }
+
+        //Time: O(N)
+        public void Union(int x, int y)
+        {
+            var rootX = Find(x);
+            var rootY = Find(y);
+            if (rootX != rootY)
+            {
+                for (int i = 0; i < _vertices.Length; i++)
+                    if (_vertices[i] == rootY)
+                        _vertices[i] = rootX;
+            }
+        }
+
+        //Time: O(1)
+        public bool IsConnected(int x, int y)
+        {
+            return Find(x) == Find(y);
+        }
+    }
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,5 +1,5 @@
-using DataStructures.Lists;
 using System;
+using DataStructures.DisjointSet;
 
 namespace DataStructures
 {
@@ -7,14 +7,20 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            var list = new SinglyLinkedList();
-            list.AddLast(1);
-            list.AddLast(2);
-            list.AddLast(3);
-            list.AddLast(2);
-            list.AddLast(1);
-            var res = list.IsPalindrome();
-            Console.WriteLine(res);
+            var uf = new FastFind(10);
+            uf.Union(0, 1);
+            uf.Union(0, 2);
+            uf.Union(1, 3);
+
+            uf.Union(4, 8);
+            uf.Union(5, 6);
+            uf.Union(5, 7);
+
+            Console.WriteLine(uf.IsConnected(0, 3));
+            Console.WriteLine(uf.IsConnected(0, 4));
+            Console.WriteLine(uf.IsConnected(0, 9));
+            uf.Union(1, 8);
+            Console.WriteLine(uf.IsConnected(3, 8));
         }
     }
 }
